b017c4b6608e9774b88829e427244df9d08114a2
Paulo Prates <paulo.prates01@hotmail.com>
2024-06-21 13:01:51 -0300

    criação do layout global

---
+++ ez-credencial/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>Logo</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    );
+}
