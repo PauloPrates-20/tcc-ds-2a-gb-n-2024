742cfaf38bb38d563b0189fb57a416c5914d13c9
Paulo Prates <paulo.prates01@hotmail.com>
2024-06-24 14:06:44 -0300

    definição da página dashboard como a página inicial do site

--- ez-credencial/pages/dashboard.jsx
+++ ez-credencial/pages/dashboard.jsx
@@ -1,9 +1,9 @@
 // import styles from "@/styles/Home.module.css";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <>
-      <h1>Home</h1>
+      <h1>Dashboard</h1>
     </>
   );
 }
