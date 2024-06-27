af0722b65840a555fd2748f3c1121f8d2bfbeec3
Paulo Prates <paulo.prates01@hotmail.com>
2024-06-27 12:55:07 -0300

    estilização da lista de eventos e teste dos separadores

--- ez-credencial/pages/dashboard.jsx
+++ ez-credencial/pages/dashboard.jsx
@@ -1,13 +1,23 @@
-// import styles from "@/styles/Home.module.css";
 import CardEvento from '@/components/CardEvento';
+import Separador from '@/components/Separador';
 import usuario from '@/public/teste-usuario';
+
+import { FaRegPenToSquare } from 'react-icons/fa6'
+
+import styles from "@/styles/Home.module.css";
 
 export default function Dashboard() {
   return (
-    <>
-      {usuario.hasOwnProperty('id') && usuario.eventos.map(evento => (
-        <CardEvento evento={evento} />
-      ))}  
-    </>
+    <div className={styles.container}>
+      <Separador tipo='button'>Adicionar evento <FaRegPenToSquare /></Separador>
+
+      <div className={styles.listas}>
+        {usuario.hasOwnProperty('id') && usuario.eventos.map(evento => (
+          <CardEvento key={evento.id} evento={evento} />
+        ))}  
+      </div>
+
+      <Separador>Relatórios</Separador>
+    </div>
   );
 }
