f762d759575cd750dadd09dd8911ddb2f5501b46
Paulo Prates <paulo.prates01@hotmail.com>
2024-06-28 12:41:08 -0300

    criação da página de adicionar eventos

---
+++ ez-credencial/pages/addevento/[id].jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+import Entrada from "@/components/Entrada";
+import Separador from "@/components/Separador";
+
+import icon from "@/public/assets/evento.png";
+
+import styles from "@/styles/AddEvento.module.css";
+
+export default function AddEvento() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <Image 
+                    src={icon}
+                    width={128}
+                    alt="Logo"
+                />
+                <p>EVENTO</p>
+            </div>
+            <div>
+                <Entrada>NOME DO EVENTO</Entrada>
+                <Entrada>DATA</Entrada>
+                <Entrada>LOCAL</Entrada>
+            </div>
+            <Separador tipo="button">ADICIONAR EVENTO</Separador>
+        </div>
+    );
+}
