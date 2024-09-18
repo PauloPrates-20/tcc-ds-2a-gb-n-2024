d1542f13772103cc41e0550ecd300a90db068043
Paulo Prates <paulo.prates01@hotmail.com>
2024-09-17 22:12:49 -0300

    Modificação das entradas para aceitar o parametro name

--- ez-credencial/components/Entrada.jsx
+++ ez-credencial/components/Entrada.jsx
@@ -1,23 +1,23 @@
 import styles from "@/styles/Entrada.module.css";
 
-export default function Entrada({ tipo, children, disable }) {
+export default function Entrada({ nome, tipo, children, disable }) {
   let input = (
-    <input className={styles.entrada} type='text' placeholder={children} />
+    <input className={styles.entrada} type='text' placeholder={children} name={nome} />
   );
 
   if (disable) {
     input = (
-      <input className={styles.entrada} placeholder={children} disabled />
+      <input className={styles.entrada} placeholder={children} name={nome} disabled />
     );
   }
 
   if (tipo) {
     input = (
-      <input className={styles.entrada} type={tipo} placeholder={children} />
+      <input className={styles.entrada} type={tipo} placeholder={children} name={nome} />
     );
 
     if (disable) {
-      <input className={styles.entrada} type={tipo} placeholder={children} disabled />
+      <input className={styles.entrada} type={tipo} placeholder={children} name={nome} disabled />
     }
   }
 
