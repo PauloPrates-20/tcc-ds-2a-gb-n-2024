6e9b14b4f8c16fb623c5a281cb73e08c1ffbd8d5
Paulo Prates <paulo.prates01@hotmail.com>
2024-09-17 22:14:31 -0300

    Adição de elemento de formulário e função de coleta de dados à pagina de cadastro

--- ez-credencial/pages/cadastro.jsx
+++ ez-credencial/pages/cadastro.jsx
@@ -7,6 +7,16 @@ import icon from "@/public/assets/cadastro-empresa.png";
 import Separador from "@/components/Separador";
 
 export default function Cadastro() {
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const formulario = new FormData(e.target);
+        const dados = {};
+        for (const [key, value] of formulario.entries()) {
+            dados[key] = value;
+        }
+        console.log(dados);
+    }
     return (
         <div className={styles.container}>
             <div className={styles.header}>
@@ -17,15 +27,17 @@ export default function Cadastro() {
                 />
                 <p>CADASTRO</p>
             </div>
-            <div className={styles.form}>
-                <Entrada>NOME</Entrada>
-                <Entrada tipo="password">SENHA</Entrada>
-                <Entrada tipo="password">CONFIRMAR SENHA</Entrada>
-                <Entrada>CNPJ</Entrada>
-                <Entrada>TELEFONE</Entrada>
-                <Entrada>ENDEREÇO</Entrada>
-            </div>
-            <Separador tipo="button">CADASTRAR</Separador>
+            <form onSubmit={handleSubmit} className={styles.formRow}>
+                <div className={styles.form}>
+                    <Entrada nome='email'>EMAIL</Entrada>
+                    <Entrada nome='senha' tipo="password">SENHA</Entrada>
+                    <Entrada nome='confirmarSenha' tipo="password">CONFIRMAR SENHA</Entrada>
+                    <Entrada nome='nome'>NOME</Entrada>
+                    <Entrada nome='cnpj'>CNPJ</Entrada>
+                    <Entrada nome='telefone'>TELEFONE</Entrada>
+                </div>
+                <Separador tipo="submit">CADASTRAR</Separador>
+            </form>
         </div>
     );
 }
