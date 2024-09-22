b9b8930a5a94aa46ae407662a23b5b34d0324eda
Paulo Prates <paulo.prates01@hotmail.com>
2024-09-21 23:54:48 -0300

    Refatoramento do componente de entrada de formulário

--- ez-credencial/app/components/Entrada.jsx
+++ ez-credencial/app/components/Entrada.jsx
@@ -1,25 +1,13 @@
 import styles from "@/styles/Entrada.module.css";
 
 export default function Entrada({ nome, tipo, children, disable }) {
-  let input = (
-    <input className={styles.entrada} type='text' placeholder={children} name={nome} />
-  );
-
-  if (disable) {
-    input = (
-      <input className={styles.entrada} placeholder={children} name={nome} disabled />
-    );
-  }
-
-  if (tipo) {
-    input = (
-      <input className={styles.entrada} type={tipo} placeholder={children} name={nome} />
-    );
-
-    if (disable) {
-      <input className={styles.entrada} type={tipo} placeholder={children} name={nome} disabled />
-    }
-  }
-
-  return input;
+	return (
+		<input 
+			className={styles.entrada}
+			type={tipo ? tipo : 'text'}
+			placeholder={children}
+			name={nome}
+			disabled={disable ? disable : false}
+		/>
+	);
 }
