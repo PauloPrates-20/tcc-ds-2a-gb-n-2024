076087eb09bb9832ed3f0c1361f0d3e3189e356b
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-04 13:03:35 -0300

    Styles: Reestilização dos Cards de evento

--- ez-credencial/app/components/CardEvento.jsx
+++ ez-credencial/app/components/CardEvento.jsx
@@ -1,16 +1,23 @@
 'use client';
 
 import { FaRegTrashCan, FaRegPenToSquare, FaRegFolderOpen } from 'react-icons/fa6';
-
 import styles from '@/styles/CardEvento.module.css';
 import Link from 'next/link';
+import { deletarEvento } from '../lib/actions';
 
-export default function CardEvento({ evento }) {
+export default function CardEvento({ evento, idUsuario }) {
+    async function handleClick() {
+        const resposta = await deletarEvento(idUsuario, evento.id);
+        console.log(resposta);
+    }
+
     return (
         <div className={styles.moldura}>
-            <p>{evento.nome}</p>
+            <p>{evento.dados.nome}</p>
             <div className={styles.acoes}>
-                <FaRegTrashCan title='EXCLUIR' className={styles.icone} />
+                <button onClick={handleClick} className={styles.icone}>
+                    <FaRegTrashCan title='EXCLUIR' />
+                </button>
                 <Link title='VISUALIZAR' className={styles.icone} href={`/dashboard/eventos/${evento.id}`}>
                     <FaRegFolderOpen />
                 </Link>
