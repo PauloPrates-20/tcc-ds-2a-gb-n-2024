827a0de5cc0c7654853531b54f4edfa5aaa40aad
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-04 22:40:10 -0300

    Exibição de dados de evento

--- ez-credencial/app/components/Moldura.jsx
+++ ez-credencial/app/components/Moldura.jsx
@@ -1,19 +1,18 @@
-import { FaPenToSquare, FaRegTrashCan, FaRegFolderClosed } from 'react-icons/fa6';
-
+import { FaPenToSquare, FaRegFolderClosed } from 'react-icons/fa6';
 import Etiqueta from './Etiqueta';
 import styles from '@/styles/Moldura.module.css';
 import Link from 'next/link';
+import BotaoExcluir from './BotaoExcluir';
 
-
-export default function Moldura({ titulo, children, voltar, idEvento }) {
+export default function Moldura({ titulo, children, voltar, idEvento, idUsuario }) {
     return (
         <div className={styles.detalhes}>
             <Etiqueta corFundo='#fff' corTexto='#2c6a68'>{titulo}</Etiqueta>
             <div className={styles.texto}>
-                <p>{children}</p>
+                <div>{children}</div>
             </div>
             <div className={styles.icons}>
-                <FaRegTrashCan title='EXCLUIR' />
+                <BotaoExcluir idAlvo={idEvento} idUsuario={idUsuario} tipoAlvo='evento' />
                 {voltar && (
                     <Link title='VOLTAR' href='/dashboard'>
                         <FaRegFolderClosed />
