431fc2c0832351c7c42fe720c8cd769f2614afaf
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-05 15:44:05 -0300

    Correção: corrigida a redenrização da página de eventos após a exlusão

--- ez-credencial/app/dashboard/eventos/[id]/page.jsx
+++ ez-credencial/app/dashboard/eventos/[id]/page.jsx
@@ -18,10 +18,10 @@ export default async function Evento({ params }) {
 		<div className={styles.container}>
 			<div className={styles.info}>
 				<div className={styles.moldura}>
-					<Moldura titulo='EVENTO' voltar={true} idEvento={evento.id} idUsuario={idUsuario}>
-						<h1>{evento.dados.nome}</h1>
-						<p>DATA: {evento.dados.horario}</p>
-						<p>Local: {evento.dados.local}</p>
+					<Moldura titulo='EVENTO' voltar={true} idEvento={evento?.id} idUsuario={idUsuario}>
+						<h1>{evento?.dados?.nome}</h1>
+						<p>DATA: {evento?.dados?.horario}</p>
+						<p>Local: {evento?.dados?.local}</p>
 					</Moldura>
 				</div>
 				<div className={styles.moldura}>
@@ -30,7 +30,7 @@ export default async function Evento({ params }) {
 			</div>
 			<div className={styles.actions}>
 				<BotaoNav url=''>COMPARTILHAR</BotaoNav>
-				<BotaoNav url={`/dashboard/relatorios/${evento.id}`}>RELATÓRIOS</BotaoNav>
+				<BotaoNav url={`/dashboard/relatorios/${evento?.id}`}>RELATÓRIOS</BotaoNav>
 			</div>
 		</div>
 	);
