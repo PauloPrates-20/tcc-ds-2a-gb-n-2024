94f413c923827c915cbd42d28676328712fad41f
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-08 12:55:38 -0300

    Refatoramento: alteração do nome padrão da função

--- app/components/Etiqueta.jsx
+++ app/components/Etiqueta.jsx
@@ -1,6 +1,6 @@
 import styles from '@/styles/Etiqueta.module.css';
 
-export default function Separador({ children, corFundo, corTexto }) {
+export default function Etiqueta({ children, corFundo, corTexto }) {
 	return (
 		<p style={{ backgroundColor: corFundo ? corFundo : '#2c6a68', color: corTexto ? corTexto : '#fff' }} className={styles.tag}>
 			{children}
