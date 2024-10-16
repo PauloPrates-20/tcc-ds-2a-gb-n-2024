aff7eb93ee11bd2e41d03a2c22de0a2eab45429a
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-15 21:51:34 -0300

    Funcionalidade: listagem de empresas na tela de compartilhamento

---
+++ app/components/ListaEmpresas.jsx
@@ -0,0 +1,26 @@
+import { lerEmpresas } from '../lib/firebase/firestoreQuerys';
+import BotaoCompartilhar from './BotaoCompartilhar';
+import BotaoExcluir from './BotaoExcluir';
+import styles from '@/styles/ListaEmpresas.module.css';
+
+export default async function ListaEmpresas({ idEvento, codigo }) {
+	const queryEmpresas = await lerEmpresas(idEvento);
+	const empresas = queryEmpresas.status ? queryEmpresas.empresas : null;
+
+	return (
+		<ul className={styles.lista}>
+			{empresas.map(empresa => (
+				<li className={styles.item} key={empresa.id}>
+					<div className={styles.dados}>
+						<span>{empresa.dados.nome}</span>
+						<span>{empresa.dados.cnpj}</span>
+					</div>
+					<div className={styles.acoes}>
+						<BotaoExcluir cor='#fff' idEvento={idEvento} idEmpresa={empresa.id} tipoAlvo='empresa' />
+						<BotaoCompartilhar cor='#fff' codigo={codigo} />
+					</div>
+				</li>
+			))}
+		</ul>
+	);
+}
