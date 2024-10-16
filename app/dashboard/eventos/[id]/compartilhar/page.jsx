f0244a3e60e588863fe14b7b3ccf86da26dda6e0
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-15 21:13:52 -0300

    Refatoramento: alterada a forma como os eventos são lidos na página de compartilhamento

--- app/dashboard/eventos/[id]/compartilhar/page.jsx
+++ app/dashboard/eventos/[id]/compartilhar/page.jsx
@@ -2,16 +2,21 @@ import FormEmpresa from '@/app/components/FormEmpresa';
 import Etiqueta from '@/app/components/Etiqueta';
 import Moldura from '@/app/components/Moldura';
 import styles from '@/styles/Compartilhar.module.css';
+import ListaEmpresas from '@/app/components/ListaEmpresas';
+import { lerEvento } from '@/app/lib/firebase/firestoreQuerys';
 
-export default function Compartilhar({ params }) {
+export default async function Compartilhar({ params }) {
+	const queryEvento = await lerEvento(params.id);
+	const evento = queryEvento.status ? queryEvento.evento : null;
+
     return (
         <div className={styles.container}>
             <div className={styles.form}>
-                <Etiqueta>CADASTRO DE EMPRESAS  </Etiqueta>
-                <FormEmpresa idEvento={params.id} />
+                <Etiqueta>CADASTRO DE EMPRESAS</Etiqueta>
+                <FormEmpresa idEvento={evento.id} />
             </div>
             <Moldura titulo='Empresas'>
-                <p>Reservado para a lista de empresas</p>
+                <ListaEmpresas idEvento={evento.id} codigo={evento.dados.codigo} />
             </Moldura>
         </div>
     );
