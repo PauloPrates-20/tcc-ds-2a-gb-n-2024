f8d23687b0b30d93293e59a4d928598b9ef14e6b
MeuAthelie <Victor_oliveira0102@hotmail.com>
2024-10-19 17:48:58 -0300

    feat: redirecionamento de empresas

--- auth.config.js
+++ auth.config.js
@@ -5,14 +5,23 @@ export const authConfig = {
 	},
 	callbacks: {
 		authorized({ auth, request: { nextUrl } }) {
-			const rotasPublicas = ['/login', '/cadastro'];
+			const rotasPublicas = [
+				'/login',
+				'/cadastro',
+			];
 			const estaAutenticado = !!auth?.user;
 			const convidado = !!auth?.user?.email?.includes('convidado');
 			const idEvento = auth?.user?.email?.split('_')[1];
 			const estaEmRotaPublica = rotasPublicas.includes(nextUrl.pathname) || /^\/dashboard\/eventos\/[^/]+\/convite$/.test(nextUrl.pathname);
+			const rotaEvento = new RegExp(`/dashboard/eventos/${idEvento}/cadastro-funcionario`);
 
 			if (!estaEmRotaPublica) {
-				if (estaAutenticado) return true;
+				if (estaAutenticado) {
+					if (convidado && !/^\/dashboard\/eventos\/[^/]+\/cadastro-funcionario$/.test(nextUrl.pathname)) {
+						return Response.redirect(new URL(`/dashboard/eventos/${idEvento}/cadastro-funcionario`, nextUrl));
+					}
+					return true;
+				}
 				return false // Produção
 				// return true; // Para teste
 			} else if (estaAutenticado) {
