72feb72301949595c6ce49e94afdcfba8363bab5
MeuAthelie <Victor_oliveira0102@hotmail.com>
2024-10-26 18:32:01 -0300

    feat: navegação através do card de relatório

--- app/components/CardRelatorio.jsx
+++ app/components/CardRelatorio.jsx
@@ -1,13 +1,14 @@
 import { FaRegFileLines } from 'react-icons/fa6';
 
 import styles from '@/styles/CardRelatorio.module.css';
+import Link from 'next/link';
 
-export default function CardRelatorio({ titulo }) {
+export default function CardRelatorio({ titulo, idEvento }) {
   return (
-    <div className={styles.moldura}>
+    <Link href={`/dashboard/relatorios/${idEvento}`} className={styles.moldura}>
       <p>Relatório</p>
       <p className={styles.titulo}>{titulo}</p>
       <FaRegFileLines className={styles.icone} />
-    </div>
+    </Link>
   );
 }
