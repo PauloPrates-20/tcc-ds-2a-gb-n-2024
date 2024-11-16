be9c1c3b49557384b0b85e98249ef7e210e8349b
MeuAthelie <Victor_oliveira0102@hotmail.com>
2024-11-16 16:11:00 -0300

    raw: changed project font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from '@/app/components/Navbar'
 import '@/styles/globals.css';
+import { Arvo } from 'next/font/google';
 
 export const metadata = {
   title: {
@@ -8,11 +9,17 @@ export const metadata = {
   },
 };
 
+const arvo = Arvo({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700'] 
+})
+
 export const revalidate = 300;
 
 export default function RootLayout({ children }) {
     return (
-      <html lang='en'>
+      <html lang='en' className={arvo.className}>
         <body>
             <Navbar />
             {children}
