0c6ff78af48aa4b0fede82a064304b016f2002d0
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-23 21:11:03 -0300

    fix: css flaws pointed by teacher

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,35 +8,37 @@ import { usePathname } from 'next/navigation';
 import Logout from './Logout';
 
 export default function Navbar() {
-    const pathname = usePathname();
-		return (
-			<nav className={styles.navbar}>
-				<ul>
-					<div className={styles.nav}>
-						<li>
-							<Link href='/dashboard'>
-								<Image 
-									src={icon}
-									width={64}
-									height={64}
-									alt=''
-								/>
-							</Link>
-						</li>
-						{(pathname !== '/login' && pathname !== '/cadastro' && !/^\/dashboard\/eventos\/[^/]+\/convite$/.test(pathname)) && (
-							<li>
-								<Link className={styles.link} href='/dashboard'>Início</Link>
-							</li>
-						)}
-					</div>
+	const pathname = usePathname();
+	const urlInicio = pathname.includes('/dashboard') ? '/dashboard' : '/login';
+	console.log(pathname);
+	return (
+		<nav className={styles.navbar}>
+			<ul>
+				<div className={styles.nav}>
 					<li>
-						{(pathname === '/login' || pathname === '/cadastro' || /^\/dashboard\/eventos\/[^/]+\/convite$/.test(pathname)) ? (
-							<Link className={styles.link} href='/login'>Início</Link>
-						) : (
-							<Logout />
-						)}
+						<Link className={styles.link} href={urlInicio}>Início</Link>
 					</li>
-				</ul>
-			</nav>
-		);
+				</div>
+				<li>
+					<Image
+						src={icon}
+						width={64}
+						height={64}
+						alt=''
+					/>
+				</li>
+				<li>
+					{pathname === '/login' ? (
+						<Link className={styles.link} href='/cadastro'>Cadastro</Link>
+					) : pathname === '/cadastro' ? (
+						<Link className={styles.link} href='/login'>Login</Link>
+					) : /^\/dashboard\/eventos\/[^/]+\/convite$/.test(pathname) ? (
+						<Link className={styles.link} href='/login'>Login</Link>
+					) : (
+						<Logout />
+					)}
+				</li>
+			</ul>
+		</nav>
+	);
 }
